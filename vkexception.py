07f532efb212476dabbcc0990a2510f8fa21cc96
Dmitrii Rumyancev <rychanya@gmail.com>
2020-02-27 02:30:46 +0300

    init

---
+++ vkexception.py
@@ -0,0 +1,17 @@
+class VKBaseException(Exception):
+    pass
+
+class VKPermissionDenied(VKBaseException):
+    pass
+
+class VKTooManyRequests(VKBaseException):
+    pass
+
+class VKUserWasDeletedOrBanned(VKBaseException):
+    pass
+
+class VKOtherException(VKBaseException):
+    pass
+
+
+#"{7: 'Permission to perform this action is denied', 6: 'Too many requests per second', 18: 'User was deleted or banned'}"
